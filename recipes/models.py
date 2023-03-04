6c715ee3f1c13c3e3da53c81244c1da92cbb81fe
CarlosVSB <carlosvictor-87@hotmail.com>
2023-03-04 15:18:10 -0300

    Migração de models para o sqlite

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
 
 
 class Recipe(models.Model):
@@ -17,3 +22,7 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True)
