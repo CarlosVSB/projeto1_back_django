92abcce3413fc4f187e9ab9eec75cc490043fa55
CarlosVSB <carlosvictor-87@hotmail.com>
2023-03-05 15:59:02 -0300

    Autor e cetgoria nula, e formas iniciais de 404

--- recipes/views.py
+++ recipes/views.py
@@ -1,21 +1,36 @@
 # from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_list_or_404
 
 from recipes.models import Recipe
 from utils.recipes.factory import make_recipe
 
 
 def home(req):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(req, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 
 def category(req, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(req, 'recipes/pages/home.html', context={
+    # recipes = Recipe.objects.filter(
+    #     category__id=category_id,
+    #     is_published=True
+    # ).order_by('-id')
+
+    # if not recipes:
+    #     raise Http404('Not found 😓')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True
+        ).order_by('-id')
+    )
+
+    return render(req, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category |',
     })
 
 
