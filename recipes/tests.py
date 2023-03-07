892311c92f02862eead57462816deac1606724a1
CarlosVSB <carlosvictor-87@hotmail.com>
2023-03-07 17:51:04 -0300

    Primeiros testes

--- recipes/tests.py
+++ recipes/tests.py
@@ -7,5 +7,13 @@ class RecipeURLsTest(TestCase):
         assert 1 == 1
 
     def test_recipe_home_url_is_correct(self):
-        home_url = reverse('recipes:home')
-        self.assertEqual(home_url, '/')
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
+
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
+
+    def test_recipe_recipe_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
