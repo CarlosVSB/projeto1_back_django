d44a072468886ba60ec18e64fe5dd5ca3e670bdc
CarlosVSB <carlosvictor-87@hotmail.com>
2023-03-07 17:42:54 -0300

    Configuração inicial pytest e primeiros testes

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class RecipeURLsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        assert 1 == 1
+
+    def test_recipe_home_url_is_correct(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
