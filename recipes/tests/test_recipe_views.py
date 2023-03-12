912f885145af79a3cafcc9f1dc4d62e5b0527555
CarlosVSB <carlosvictor-87@hotmail.com>
2023-03-12 14:44:31 -0300

    Separando pacotes de testes

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -4,23 +4,6 @@ from django.urls import resolve, reverse
 from recipes import views
 
 
-class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        assert 1 == 1
-
-    def test_recipe_home_url_is_correct(self):
-        url = reverse('recipes:home')
-        self.assertEqual(url, '/')
-
-    def test_recipe_category_url_is_correct(self):
-        url = reverse('recipes:category', kwargs={'category_id': 1})
-        self.assertEqual(url, '/recipes/category/1/')
-
-    def test_recipe_recipe_url_is_correct(self):
-        url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(url, '/recipes/1/')
-
-
 class RecipeViewsTest(TestCase):
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
