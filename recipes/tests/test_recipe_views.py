b55d3678de1e614037f216065da9e533e9409bf5
CarlosVSB <carlosvictor-87@hotmail.com>
2023-03-12 15:45:08 -0300

    Criando fixture

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve, reverse
 
 from recipes import views
+from recipes.models import Category, Recipe, User
 
 
 class RecipeViewsTest(TestCase):
@@ -23,6 +24,31 @@ class RecipeViewsTest(TestCase):
             '<h1>No recipes found!</h1>',
             response.content.decode('utf-8')
         )
+
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Categoria')
+        author = User.objects.create(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+        )
+        assert 1 == 1
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category',
