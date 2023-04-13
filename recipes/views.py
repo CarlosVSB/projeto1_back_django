ba8e7553d006cc9f1365c58a9d3316bddfbb8f95
CarlosVSB <carlosvictor-87@hotmail.com>
2023-04-13 18:03:53 -0300

    Implementação do mecânismo de busca

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
@@ -41,7 +42,13 @@ def search(req):
     if not search_term:
         raise Http404()
 
+    recipes = Recipe.objects.filter(
+        Q(title__icontains=search_term) |
+        Q(description__icontains=search_term),
+    ).order_by('-id')
+
     return render(req, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}"',
         'search_term': search_term,
+        'recipes': recipes
     })
