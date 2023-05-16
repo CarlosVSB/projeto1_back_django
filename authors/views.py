251408bc41182ac66dd5edffa0ca27470b1945cd
CarlosVSB <carlosvictor-87@hotmail.com>
2023-05-16 16:51:01 -0300

    Criação de view para lidar com post

--- authors/views.py
+++ authors/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import redirect, render
 
 from .forms import RegisterForm
 
@@ -6,10 +7,19 @@ from .forms import RegisterForm
 
 
 def register_view(req):
-    if req.POST:
-        form = RegisterForm(req.POST)
-    else:
-        form = RegisterForm()
+    register_form_data = req.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
     return render(req, 'authors/pages/register_view.html', {
-        'form': form
+        'form': form,
     })
+
+
+def register_create(req):
+    if not req.POST:
+        raise Http404()
+
+    POST = req.POST
+    req.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')
