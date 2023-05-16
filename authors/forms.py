418277744e92dd14582df1a207fa035f4c63675b
CarlosVSB <carlosvictor-87@hotmail.com>
2023-05-16 17:28:48 -0300

    Configurando o formulário

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,27 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+        # Utilizar exlcude = ['campo1','campo2'] retorna todos os campos menos o campo1 e campo2
+        labels = {
+            'username': 'Username',
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'email': 'Email',
+            'password': 'Password',
+        }
+        help_texts = {
+            'email': 'The e-mail must be valid',
+        }
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+            }
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your first name here',
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            }),
+        }
